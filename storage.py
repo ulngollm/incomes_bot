09eea92e48bf2da9d048818a8ec486260008e6aa
ulngollm <ulngollm@gmail.com>
2023-04-03 14:20:51 +0300

    добавлена обработка пустого ответа от бд

--- storage.py
+++ storage.py
@@ -9,10 +9,11 @@ class Storage:
         self.repo.add(income.owner_id, income.name, income.value, income.date)
 
     def get_today_sum(self, user_id) -> int:
-        return self.repo.get_today(user_id)
+        result = self.repo.get_today(user_id)
+        return result if result != None else 0
     
     def get_today_list(self, user_id) -> list:
         result = self.repo.get_today_list(user_id)
         return ["%+d\t%s" % (x[3], x[2]) for x in result]
-    
+     
     
